4fd68119c87acedc92c3d9322da8221b32d80676
HoangDucHung <hunglasoone@gmail.com>
2022-01-13 09:57:21 +0700

    update src

---
+++ src/pages/news.js
@@ -0,0 +1,11 @@
+import NewsList from "../components/newsList";
+
+const NewsPage = {
+    render() {
+        return `
+            
+            ${NewsList.render()}
+        `;
+    },
+};
+export default NewsPage;
